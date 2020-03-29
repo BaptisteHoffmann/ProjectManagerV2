d6886341dc6f56879b51aa78a9b99e065fe322da
AhmedALSGit <ahmed.al_sawah@master-stic.fr>
2020-03-29 23:37:26 +0200

    server change

--- express-server/servernd.js
+++ express-server/servernd.js
@@ -27,11 +27,7 @@ app.use(function (req, res, next) {
 
     // Request headers you wish to allow
     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-<<<<<<< Updated upstream
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-Access-Token, Content-Type, Accept");
-=======
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
->>>>>>> Stashed changes
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
